ac637dd08e740b8b2f557f365b5624666abffd11
jeff-goldstein <jeff.goldstein@sparkpost.com>
2016-11-23 20:14:54 -0800

    Update cgBuildSubmitStored.php

--- cgBuildSubmitStored.php
+++ cgBuildSubmitStored.php
@@ -46,16 +46,6 @@ function cleanup()
 	if (location > 0) {returnpath.value = returnpath.value.substring(0, location)};
 }
 </script>
-<script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-  ga('create', 'UA-86406300-1', 'auto');
-  ga('send', 'pageview');
-
-</script>
 <style>
     /* This expands the text for more room while typing */
     input[type=previewEmailEntries] 
